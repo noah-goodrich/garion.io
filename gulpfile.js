70948392ef5b59120d15c610aa4b7d0c0379a2f7
Noah Goodrich <me@noahgoodrich.com>
2014-06-17 20:34:00 -0600

    Got html2js working
    All assets are building through gulp and are returning correctly to the
    browser

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,22 @@ var gulp = require('gulp');
 // Include Our Plugins
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
+var html2js = require('gulp-html2js');
 var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 
 var paths = {
+  index: './public/src/index.html',
+  html: ['./public/src/html/*.html', './public/src/html/**/*.html'],
   scss: './public/src/scss/*.scss',
   scripts: ['./public/src/js/app.js', './public/src/js/**/*.js']
 };
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('./public/src/js/**/*.js')
+    return gulp.src(paths.scripts)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
@@ -27,9 +30,21 @@ gulp.task('clean', function() {
     .pipe(clean());
 });
 
+// Compile html2js
+gulp.task('html', function () {
+  return gulp.src(paths.html)
+    .pipe(html2js({
+      base: 'public/src',
+      outputModuleName: 'garion',
+      useStrict: true
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('./public/dist/js'));
+});
+
 // Copy index.html
 gulp.task('index', function() {
-  return gulp.src('./public/src/index.html')
+  return gulp.src(paths.index)
     .pipe(gulp.dest('./public/dist'));
 });
 
@@ -56,7 +71,7 @@ gulp.task('sass', function() {
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-  gulp
+    gulp
     .src([
       './public/bower/jquery/query.js',
       './public/bower/angular/angular.js',
@@ -73,11 +88,12 @@ gulp.task('scripts', function() {
 });
 
 // Default Task
-gulp.task('default', ['lint', 'index', 'sass', 'scripts']);
+gulp.task('default', ['lint', 'html', 'index', 'sass', 'scripts']);
 
 // Watch Files For Changes
 gulp.task('watch', ['default'], function() {
-    gulp.watch('./public/src/index.html', ['index']);
-    gulp.watch(paths.scripts, ['lint', 'scripts']);
-    gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.html, ['html']);
+  gulp.watch(paths.index, ['index']);
+  gulp.watch(paths.scripts, ['lint', 'scripts']);
+  gulp.watch(paths.sass, ['sass']);
 });
--- public/dist/js/app.js
+++ public/dist/js/app.js
@@ -2,8 +2,8 @@ angular.module('garion', ['ngRoute','ngSanitize','angular-data.DS'])
 .config(['$routeProvider', function($routeProvider) {
 
   $routeProvider.when('/', {
-    templateUrl: 'html/indexController.html', 
-    controller: 'IndexController', 
+    templateUrl: 'html/indexController.html',
+    controller: 'IndexController',
     controllerAs: 'IdxCtrl'
   });
 }]);
